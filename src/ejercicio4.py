ad110a07577f2526f225d0229b3ed45c09ea7f3f
Cande <cerusecandelaria@gmail.com>
2022-05-21 18:01:40 -0300

    Subo ej 4

---
+++ src/ejercicio4.py
@@ -0,0 +1,46 @@
+################
+# Candelaria Ceruse - @CandeCeruse
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+"""
+4. Suma lenta
+
+Escribir una función que haga la suma entre dos números enteros sin hacer la operación de manera directa. Esto quiere decir que para hacer la suma entre 4 y 3, las operaciones resultantes deberán ser 4+1+1+1.
+
+La funcion debe ser capaz de sumar cualquier numero entero positivo y negativo.
+"""
+#Funciones del ejercicio
+
+def suma_lenta(numero, otro_numero):
+    '''
+    Si otro_numero es positivo, todo bien.
+    Sino que en vez de sumar, lo reste.
+    '''
+    if otro_numero < 0 :
+        print(f"{numero} + ({otro_numero})")
+        otro_numero=otro_numero * -1
+        for i in range(otro_numero):
+            numero=numero-1
+            print(f"+ (-1) = {numero}")
+    else:
+        print(f"{numero} + {otro_numero}")
+        for i in range(otro_numero):
+            numero=numero+1
+            print(f"+1 = {numero}")
+            
+    print(f"Total = {numero}")
+
+def principal():
+    """
+    Despues de ingresar dos numeros, la funcion suma_lenta los sumara
+    de a uno. Si el segundo numero es negativo la suma se convierte en
+    una resta.
+    """
+    numero_a = int(input("Ingrese numero a: "))
+    numero_b = int(input("Ingrese numero b: "))
+    suma_lenta(numero_a, numero_b)
+    pass
+
+if __name__ == "__main__":
+    principal()
