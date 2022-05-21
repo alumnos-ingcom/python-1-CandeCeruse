d4bcaac05e44e23c11b1a27b4041ca5d3567fbb8
Cande <cerusecandelaria@gmail.com>
2022-05-21 16:37:47 -0300

    Agrego ejercicio 2

---
+++ src/ejercicio2.py
@@ -0,0 +1,40 @@
+################
+# Candelaria Ceruse - @CandeCeruse
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+"""
+2. Números positivos y negativos
+
+Escribir una función que reciba un número e indique si el mismo es positivo,
+negativo o cero utilizando sumas y restas.
+"""
+
+# Reemplazar por las funciones a implementar del ejercicio
+def signo(numero):
+    numero_original = numero
+    numero_mas_1 = numero + 1
+    numero_menos_1 = numero - 1
+    if numero_mas_1 > 0:
+        if numero_menos_1 >= 0:
+            print("Es positivo")
+        else:
+            print("Es cero")
+    else:
+        print("Es negativo")
+
+def principal():
+    """
+    Al ingresar un numero la funcion signo detecta si es positivo,
+    negativo, o cero. Lo hace comparando dos nuevos valores, el numero mas 1
+    y el numero menos 1.
+    Si el numero mas 1 es negativo entonces el numero es negativo. Sino queda
+    comparar si es mayor o igual a cero, si lo es entonces es positivo, y si da
+    menor que cero entonces el numero es cero.
+    """
+    numero_ingresado = int(input("Ingrese numero\n"))
+    signo(numero_ingresado)
+    pass
+
+if __name__ == "__main__":
+    principal()
