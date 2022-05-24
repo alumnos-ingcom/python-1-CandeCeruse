4fdd7d565feec71e9257a6252e2617d24013cb02
Cande <cerusecandelaria@gmail.com>
2022-05-24 18:21:19 -0300

    Subo ejercicios 6 y 7

---
+++ src/ejercicio7.py
@@ -0,0 +1,54 @@
+###############
+# Candelaria Ceruse - @CandeCeruse
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+"""
+7. Transformación de un número
+
+Escribir un programa que permita transformar un
+número solicitado expresado en horas, minutos y
+segundos a segundos a segundos. Y otra que haga
+el cambio en el sentido contrario, devolviendo una tuple.
+
+Recuerden que un grado son 60 minutos y un minuto son 60 segundos.
+"""
+# Funciones del ejercicio
+def sexadecimal_a_decimal(horas, minutos, segundos):
+    """
+    Esta funcion convierte de horas, minutos y segundos a segundos
+    """
+    horas_segundos = (horas*60)*60
+    minutos_segundos = minutos*60
+
+    suma_segundos = segundos + minutos_segundos + horas_segundos
+    return suma_segundos
+
+def decimal_a_sexadecimal(segundos):
+    """
+    Transforma los segundos a sexagesimal dividiendo cada dígito del numero
+    sexagesimal, y guardando los restos en horas, minutos y segundos
+    """
+    horas_segundos = segundos // 3600
+    resto_horas = segundos % 3600
+    minutos_segundos = resto_horas // 60
+    resto_minutos = resto_horas % 60
+    segundos_convertidos  = resto_minutos
+    resultado = (f"Resultado: {horas_segundos}hs, {minutos_segundos}', {segundos_convertidos}''")
+    return resultado
+
+def principal():
+    """
+    Este programa transforma la hora ingresada a segundos.
+    Y luego la vuelve a transformar a sexagesimal.
+    """
+    horas = int(input("Ingrese horas: "))
+    minutos = int(input("Ingrese minutos: "))
+    segundos = int(input("Ingrese segundos: "))
+    suma_segundos = sexadecimal_a_decimal(horas, minutos, segundos)
+    print(f"Segundos: {suma_segundos}")
+    resultado = decimal_a_sexadecimal(suma_segundos)
+    print(f"{resultado}")
+
+if __name__ == "__main__":
+    principal()
